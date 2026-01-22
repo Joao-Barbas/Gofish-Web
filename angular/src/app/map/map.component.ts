ffb8b6b28376a0d216342d7122b745afdf02540f
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-01-22 11:30:39 +0000

    Showing mapboxMap in interface

--- angular/src/app/map/map.component.ts
+++ angular/src/app/map/map.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@gofish/shared/services/auth.service';
 import { UserService } from '@gofish/shared/services/user.service';
+import mapboxgl from 'mapbox-gl';
+
+
 
 @Component({
   selector: 'app-map',
   imports: [],
   templateUrl: './map.component.html',
-  styles: ``
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+
+
+
+
   public firstName: string = '';
 
   constructor(
@@ -17,8 +24,18 @@ export class MapComponent implements OnInit {
     private authService: AuthService,
     private userService: UserService
   ){}
-
   ngOnInit(): void {
+
+        (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiY2F0Y2hzcG90IiwiYSI6ImNtZHltb3kzMjAzamMyanNhb3FzYjZpN20ifQ.Zc0Kd54svxaUO1PDRGm-OQ';
+        const map = new mapboxgl.Map({
+          container: 'map', // container ID
+          style: 'mapbox://styles/mapbox/streets-v12', // style URL
+          center: [-74.5, 40], // starting position [lng, lat]
+          zoom: 4, // starting zoom
+        });
+
+
+
     this.userService.getUserProfile().subscribe({
       next: (res: any) => this.firstName = res.firstName,
       error: (err: any) => console.log("Error while retrieving user profile:\n", err)
@@ -29,4 +46,6 @@ export class MapComponent implements OnInit {
     this.authService.deleteToken();
     this.router.navigateByUrl('');
   }
+
+
 }
