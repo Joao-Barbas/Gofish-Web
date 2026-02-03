61e14066853f1f219c36ff822457993bdad34863
RicardoNogueiraReis <ricardovreis499@gmail.com>
2026-02-03 19:20:17 +0000

    Página do about-us está feita. Adicionei os icones que a página precisava (check e cross). Meti classes que estavam no home para o styles para as poder reutilizar no about-us

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@gofish/header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ HeaderComponent ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
