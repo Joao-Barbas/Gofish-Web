262aad5fdc9d7533bd0cfa62169b769f7909f236
RicardoNogueiraReis <ricardovreis499@gmail.com>
2026-02-07 16:48:58 +0000

    Agora as páginas home e about-us têm acesso ao footer.

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@gofish/header/header.component';
+import { FooterComponent } from '@gofish/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [ HeaderComponent ],
+  imports: [ HeaderComponent, FooterComponent ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
