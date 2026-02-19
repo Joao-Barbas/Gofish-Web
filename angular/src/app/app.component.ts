d06f64e55d5acb5c1ede836e07df33729eb54779
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-02-19 16:31:21 +0000

    Clean app component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { RouterOutlet } from '@angular/router';
-import { Component, inject } from '@angular/core';
-import { PopupService } from '@gofish/shared/services/popup.service';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CommonModule, RouterOutlet ],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  public readonly popupService = inject(PopupService);
   public title = 'angular';
 }
