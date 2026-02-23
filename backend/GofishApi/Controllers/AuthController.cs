ea343aa18fa5690c9c2e6d5ec012eea87c43115d
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-02-23 14:14:48 +0000

    Remove namespace indentation from auth controller

--- backend/GofishApi/Controllers/AuthController.cs
+++ backend/GofishApi/Controllers/AuthController.cs
@@ -5,105 +5,104 @@ using GofishApi.Dtos;
 using GofishApi.Models;
 using GofishApi.Services;
 
-namespace GofishApi.Controllers
+namespace GofishApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    private readonly ILogger<AuthController> _logger;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IJwtService _jwtService;
+    private readonly ITwoFactorTokenService _twoFactorService;
+
+    public AuthController(
+        ILogger<AuthController> logger,
+        UserManager<AppUser> userManager,
+        IJwtService jwtService,
+        ITwoFactorTokenService twoFactorService
+    )
     {
-        private readonly ILogger<AuthController> _logger;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IJwtService _jwtService;
-        private readonly ITwoFactorTokenService _twoFactorService;
+        _logger = logger;
+        _userManager = userManager;
+        _jwtService = jwtService;
+        _twoFactorService = twoFactorService;
+    }
 
-        public AuthController(
-            ILogger<AuthController> logger,
-            UserManager<AppUser> userManager,
-            IJwtService jwtService,
-            ITwoFactorTokenService twoFactorService
-        )
+    [HttpPost("SignUp")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpReqDTO dto)
+    {
+        var user = new AppUser
         {
-            _logger = logger;
-            _userManager = userManager;
-            _jwtService = jwtService;
-            _twoFactorService = twoFactorService;
+            Email = dto.Email,
+            UserName = dto.UserName,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName
+        };
+        try
+        {
+            IdentityResult result;
+            result = await _userManager.CreateAsync(user, dto.Password);
+            if (!result.Succeeded) { throw new IdentityException(result.Errors); }
+            result = await _userManager.AddToRoleAsync(user, "User");
+            if (!result.Succeeded) { throw new IdentityException(result.Errors); }
+            return Ok(new ApiResponse<object>());
         }
-
-        [HttpPost("SignUp")]
-        public async Task<IActionResult> SignUp([FromBody] SignUpReqDTO dto)
+        catch (IdentityException ex)
         {
-            var user = new AppUser
+            return BadRequest(new ApiErrorResponse
             {
-                Email = dto.Email,
-                UserName = dto.UserName,
-                FirstName = dto.FirstName,
-                LastName = dto.LastName
-            };
-            try
-            {
-                IdentityResult result;
-                result = await _userManager.CreateAsync(user, dto.Password);
-                if (!result.Succeeded) { throw new IdentityException(result.Errors); }
-                result = await _userManager.AddToRoleAsync(user, "User");
-                if (!result.Succeeded) { throw new IdentityException(result.Errors); }
-                return Ok(new ApiResponse<object>());
-            }
-            catch (IdentityException ex)
-            {
-                return BadRequest(new ApiErrorResponse
-                {
-                    Errors = ex.Errors
-                               .Select(e => new ApiError(e.Code, e.Description))
-                               .ToList()
-                });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ApiErrorResponse
-                {
-                    Errors = [new("UnexpectedError", ex.Message)]
-                });
-            }
-        }
-
-        [HttpPost("SignIn")]
-        public async Task<IActionResult> SignIn([FromBody] SignInReqDTO dto)
-        {
-            var user = await _userManager.FindByNameAsync(dto.EmailOrUserName)
-                    ?? await _userManager.FindByEmailAsync(dto.EmailOrUserName);
-
-            if (user is null)
-            {
-                return NotFound(new ApiErrorResponse
-                {
-                    Errors = [new("NoSuchUser", "Email or username returned no results")]
-                });
-            }
-            if (!await _userManager.CheckPasswordAsync(user, dto.Password))
-            {
-                return BadRequest(new ApiErrorResponse
-                {
-                    Errors = [new("InvalidCredentials", "Email/username or password is incorrect")]
-                });
-            }
-            if (user.TwoFactorEnabled)
-            {
-                var hasAuthenticator = await _userManager.GetAuthenticatorKeyAsync(user) is { Length: > 0 };
-                if (hasAuthenticator)
-                {
-                    var twoFactorToken = _twoFactorService.CreateTwoFactorToken(user);
-                    return Ok(new ApiResponse<SignInResDTO>
-                    {
-                        Data = new(null, true, twoFactorToken)
-                    });
-                }
-            }
-
-            var token = await _jwtService.CreateTokenAsync(user);
-            return Ok(new ApiResponse<SignInResDTO>
-            {
-                Data = new(token)
+                Errors = ex.Errors
+                           .Select(e => new ApiError(e.Code, e.Description))
+                           .ToList()
             });
         }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ApiErrorResponse
+            {
+                Errors = [new("UnexpectedError", ex.Message)]
+            });
+        }
+    }
+
+    [HttpPost("SignIn")]
+    public async Task<IActionResult> SignIn([FromBody] SignInReqDTO dto)
+    {
+        var user = await _userManager.FindByNameAsync(dto.EmailOrUserName)
+                ?? await _userManager.FindByEmailAsync(dto.EmailOrUserName);
+
+        if (user is null)
+        {
+            return NotFound(new ApiErrorResponse
+            {
+                Errors = [new("NoSuchUser", "Email or username returned no results")]
+            });
+        }
+        if (!await _userManager.CheckPasswordAsync(user, dto.Password))
+        {
+            return BadRequest(new ApiErrorResponse
+            {
+                Errors = [new("InvalidCredentials", "Email/username or password is incorrect")]
+            });
+        }
+        if (user.TwoFactorEnabled)
+        {
+            var hasAuthenticator = await _userManager.GetAuthenticatorKeyAsync(user) is { Length: > 0 };
+            if (hasAuthenticator)
+            {
+                var twoFactorToken = _twoFactorService.CreateTwoFactorToken(user);
+                return Ok(new ApiResponse<SignInResDTO>
+                {
+                    Data = new(null, true, twoFactorToken)
+                });
+            }
+        }
+
+        var token = await _jwtService.CreateTokenAsync(user);
+        return Ok(new ApiResponse<SignInResDTO>
+        {
+            Data = new(token)
+        });
     }
 }
