bfd4b6ca25b238a7e514c2a2d50665c9be9a84a1
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-02-27 10:08:33 +0000

    Creating Fixture WebAppFactory

---
+++ backend/GofishApi.Tests/Fixtutes/WebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using GofishApi.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GofishApi.Tests.Fixtutes;
+
+// Fixtures/WebAppFactory.cs
+public class WebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // 1. Remover o SQL Server real
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            // 2. Adicionar base de dados falsa em memória
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        });
+    }
+}
