e850deca179a8cf07766c9c3de4f010c8c9dda98
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-02-27 18:54:35 +0000

    Create extensions of ProblemDetais to include collection of errors
    Adds a collection of errors to support error responses like IdentityResult does. But with project-wide adoption

---
+++ backend/GofishApi/Core/ApiProblemDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GofishApi.Core;
+
+/// <summary>
+/// A machine-readable format for specifying errors in HTTP API responses based on <see href="https://tools.ietf.org/html/rfc7807"/>.
+/// </summary>
+/// <remarks>
+/// Extends <see cref="ProblemDetails"/> to support returning multiple errors at once,
+/// following the same convention/way as <see cref="IdentityResult"/>.
+/// </remarks>
+public class ApiProblemDetails : ProblemDetails
+{
+    /// <summary>
+    /// Application-level errors.
+    /// Each with a machine-readable <c>Code</c> and human-readable <c>Description</c>.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("errors")]
+    public IEnumerable<ApiError>? Errors { get; set; }
+}
