b43597bb9fd4bbc7fac7e5f27ce3c0d258e0a334
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-03-05 06:30:41 +0000

    Set azure api domain url on angular

--- angular/src/environments/environment.development.ts
+++ angular/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'https://localhost:7113/api'
+  // apiUrl: 'https://localhost:7113/api'
+  apiUrl: 'https://gofish-afdubwcnejh4apcm.spaincentral-01.azurewebsites.net/api'
 } as const;
