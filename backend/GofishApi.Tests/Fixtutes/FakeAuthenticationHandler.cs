5b6d03e32dae7277f586db2080ae8fb5f96350ca
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-03-05 10:29:31 +0000

    Create FakeAuthenticationHandler for xunit tests

---
+++ backend/GofishApi.Tests/Fixtutes/FakeAuthenticationHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Logging;
+
+namespace GofishApi.Tests.Fixtures;
+
+public class FakeAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public const string SchemeName = "FakeAuth";
+
+    public FakeAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder)
+        : base(options, logger, encoder) { }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, "test-user-id")
+        };
+
+        var identity = new ClaimsIdentity(claims, SchemeName);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, SchemeName);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
