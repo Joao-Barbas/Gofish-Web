263a21294430ca864626c9068f2fe6e127d5bbd3
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-03-12 10:04:21 +0000

    Creating GroupModel

---
+++ backend/GofishApi/Models/Group.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GofishApi.Models;
+
+public class Group
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string NormalizedName { get; set; }
+
+    [MaxLength(1000)]
+    public string Description { get; set; }
+
+    [Url]
+    [MaxLength(500)]
+    public string AvatarUrl { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+}
