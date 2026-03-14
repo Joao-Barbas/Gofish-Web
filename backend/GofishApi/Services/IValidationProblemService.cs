54bc69f6063e500d5668a06bab99c689ff219d67
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-03-14 13:53:07 +0000

    Create service to easily create problem details with errors

---
+++ backend/GofishApi/Services/IValidationProblemService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+
+namespace GofishApi.Services;
+
+public interface IValidationProblemService
+{
+    public ValidationProblem ValidationProblem(string errorCode, string errorDescription);
+    public ValidationProblem ValidationProblem(IdentityResult identityResult);
+    public ValidationProblem ValidationProblem(string propertyName, params string[] propertyErrors);
+}
---
+++ backend/GofishApi/Services/ValidationProblemService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
+
+namespace GofishApi.Services;
+
+public class ValidationProblemService : IValidationProblemService
+{
+    public ValidationProblem ValidationProblem(string errorCode, string errorDescription)
+    {
+        return TypedResults.ValidationProblem(new Dictionary<string, string[]> {{ errorCode, [errorDescription] }});
+    }
+
+    public ValidationProblem ValidationProblem(IdentityResult identityResult)
+    {
+        Debug.Assert(!identityResult.Succeeded);
+        var errors = identityResult.Errors
+        .GroupBy(e => e.Code)
+        .ToDictionary(g => g.Key, g => g.Select(e => e.Description).ToArray());
+        return TypedResults.ValidationProblem(errors);
+    }
+
+    public ValidationProblem ValidationProblem(string propertyName, params string[] propertyErrors)
+    {
+        return TypedResults.ValidationProblem(new Dictionary<string, string[]> {{ propertyName, propertyErrors } });
+    }
+}
