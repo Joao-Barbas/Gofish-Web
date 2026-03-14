f0f835004d0510a47af579a9b12f4926747cce5b
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-03-14 12:44:13 +0000

    Rebase sprint-3 into profiles

---
+++ angular/src/app/features/user/profile/pages/pins/pins.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pins',
+  imports: [],
+  templateUrl: './pins.component.html',
+  styleUrl: './pins.component.css',
+})
+export class PinsComponent {
+
+}
