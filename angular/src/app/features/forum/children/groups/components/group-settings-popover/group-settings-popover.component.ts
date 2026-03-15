87e3b5327468e997039cceaa78a8abeed5e2ed30
Ricardo Reis <48648481+RicardoNogueiraReis@users.noreply.github.com>
2026-03-15 15:19:26 +0000

    group-settings-popover done

---
+++ angular/src/app/features/forum/children/groups/components/group-settings-popover/group-settings-popover.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-settings-popover',
+  imports: [],
+  templateUrl: './group-settings-popover.component.html',
+  styleUrl: './group-settings-popover.component.css',
+})
+export class GroupSettingsPopoverComponent {
+
+}
