f695e5d5a0dec6eb9e1310373c13650f6e0bc9e1
Ricardo Reis <48648481+RicardoNogueiraReis@users.noreply.github.com>
2026-03-15 12:38:28 +0000

    Group feed now shows group member cards in grid (2 cols)

--- angular/src/app/features/forum/children/groups/children/group-members-placeholder/group-members-placeholder.component.ts
+++ angular/src/app/features/forum/children/groups/children/group-members-placeholder/group-members-placeholder.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from "../../components/user-card/user-card.component";
 
 @Component({
   selector: 'app-group-members-placeholder',
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './group-members-placeholder.component.html',
   styleUrl: './group-members-placeholder.component.css',
 })
