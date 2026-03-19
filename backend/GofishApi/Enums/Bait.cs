061a9c393c5c18e93f29d834a7ee06f49f9e3751
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-03-19 00:41:33 +0000

    Add display names to needed enums

--- backend/GofishApi/Enums/Bait.cs
+++ backend/GofishApi/Enums/Bait.cs
@@ -1,14 +1,16 @@
-﻿namespace GofishApi.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GofishApi.Enums;
 
 public enum Bait
 {
-    Unknown = 0,
-    Worm,
-    Shrimp,
-    Sardine,
-    Squid,
-    Mussel,
-    Crab,
-    LiveFish,
-    DeadFish
+    [Display(Name="Unknown")]   Unknown  = 0,
+    [Display(Name="Wor")]       Worm     = 1,
+    [Display(Name="Shrimp")]    Shrimp   = 2,
+    [Display(Name="Sardine")]   Sardine  = 3,
+    [Display(Name="Squid")]     Squid    = 4,
+    [Display(Name="Mussel")]    Mussel   = 5,
+    [Display(Name="Crab")]      Crab     = 6,
+    [Display(Name="Live fish")] LiveFish = 7,
+    [Display(Name="Dead fish")] DeadFish = 8
 }
