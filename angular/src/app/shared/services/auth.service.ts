ce843364a66353446abef81c575d83c170284759
Gonçalo Fontoura <150022847+xFontoura@users.noreply.github.com>
2026-03-20 16:06:51 +0000

    ui changes

--- angular/src/app/shared/services/auth.service.ts
+++ angular/src/app/shared/services/auth.service.ts
@@ -96,7 +96,7 @@ export class AuthService {
   readonly isUser         = computed<boolean>(() => this.hasRole('User'));
   readonly isExternalUser = computed<boolean>(() => this.loginProvider() !== 'Local');
 
-  /** @deprecated Use userName() instead */
+  
   getUserName() {
     return this.userName()!.toString();
   }
