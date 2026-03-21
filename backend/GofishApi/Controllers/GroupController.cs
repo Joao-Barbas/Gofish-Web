8e5abc5a50235dc30ba9c626a730a01b1f062aba
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-03-21 11:03:17 +0000

    Created CreateGroupEndpoint

--- backend/GofishApi/Controllers/GroupController.cs
+++ backend/GofishApi/Controllers/GroupController.cs
@@ -1,6 +1,9 @@
 ﻿using GofishApi.Data;
 using GofishApi.Dtos;
+using GofishApi.Enums;
+using GofishApi.Exceptions;
 using GofishApi.Models;
+using GofishApi.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -18,16 +21,19 @@ public class GroupController : ControllerBase
     private readonly ILogger<PostController> _logger;
     private readonly AppDbContext _db;
     private readonly UserManager<AppUser> _userManager;
+    private readonly IBlobStorageService _blobStorage;
 
     public GroupController(
         ILogger<PostController> logger,
         AppDbContext db,
-        UserManager<AppUser> userManager
+        UserManager<AppUser> userManager,
+        IBlobStorageService blobStorage
     )
     {
         _logger = logger;
         _db = db;
         _userManager = userManager;
+        _blobStorage = blobStorage;
     }
 
     [Authorize]
@@ -70,5 +76,67 @@ public class GroupController : ControllerBase
         return Ok(new GetGroupResDTO(data));
     }
 
+    [Authorize]
+    [HttpPost("CreateGroup")]
+    [RequestSizeLimit(5_000_000)]
+    public async Task<IActionResult> CreateGroup([FromForm] CreateGroupReqDTO dto)
+    {
+        var userId = User.FindFirstValue(JwtRegisteredClaimNames.Sub);
+        var allowedTypes = new[] { "image/jpeg", "image/png" };
+        string imageUrl;
+        var user = userId is null ? null : await _userManager.FindByIdAsync(userId);
+        if (user is null) return Unauthorized();
 
+        if (!allowedTypes.Contains(dto.Image.ContentType)) throw new AppException("Bad Request", "Invalid file type.", StatusCodes.Status400BadRequest);
+
+        var normalizedName = dto.Name.Trim().ToUpper();
+        var exists = await _db.Groups.AnyAsync(g => g.NormalizedName == normalizedName);
+        if (exists) return BadRequest("Já existe um grupo com esse nome.");
+
+        try
+        {
+            imageUrl = await _blobStorage.UploadImageAsync(dto.Image);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error during image upload to blob storage");
+            throw new AppException("Service Unavailable", "Image upload failed.", StatusCodes.Status503ServiceUnavailable);
+        }
+
+        var newGroup = new Group
+        {
+            Name = dto.Name.Trim(),
+            NormalizedName = normalizedName,
+            Description = dto.Description?.Trim() ?? string.Empty,
+            AvatarUrl = imageUrl,
+            CreatedAt = DateTime.UtcNow,
+        };
+
+        try
+        {
+            _db.Groups.Add(newGroup);
+            await _db.SaveChangesAsync();
+
+            var groupUser = new GroupUser
+            {
+                GroupId = newGroup.Id,
+                UserId = user.Id,
+                RoleId = "1"
+            };
+
+            _db.GroupUsers.Add(groupUser);
+            await _db.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error while saving group");
+            throw new AppException(
+                "Service Unavailable",
+                $"Unable to save this group: {ex.InnerException?.Message ?? ex.Message}",
+                StatusCodes.Status503ServiceUnavailable
+            );
+        }
+
+        return Ok(new CreateGroupResDTO(newGroup.Id));
+    }
 }
