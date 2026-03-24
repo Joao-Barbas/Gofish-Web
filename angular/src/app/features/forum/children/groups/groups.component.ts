c5cb4c5d3fd2a97a0430ecc326125dba08a82489
Gonçalo Fontoura <150022847+xFontoura@users.noreply.github.com>
2026-03-24 09:41:29 +0000

    group details routes

--- angular/src/app/features/forum/children/groups/groups.component.ts
+++ angular/src/app/features/forum/children/groups/groups.component.ts
@@ -1,8 +1,10 @@
 import { filter } from 'rxjs';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Path } from '@gofish/shared/constants';
+import { GetGroupDTO, GetGroupReqDTO, GetGroupResDTO, GetUserGroupsResDTO } from '@gofish/shared/dtos/group.dto';
+import { GroupsService } from '@gofish/shared/services/groups.service';
 
 
 type NavPath = {
@@ -17,25 +19,46 @@ type NavPath = {
   styleUrl: './groups.component.css',
 })
 export class GroupsComponent {
-    private readonly router: Router = inject(Router);
-    public currentPath: WritableSignal<string> = signal(this.router.url);
+  private readonly route= inject(ActivatedRoute);
+  private readonly groupsService = inject(GroupsService);
+  private readonly router: Router = inject(Router);
+  public currentPath: WritableSignal<string> = signal(this.router.url);
+  group = signal<GetGroupResDTO | null>(null);
 
-    public navPaths: NavPath[] = [
-      { path: 'group-post-redirect-testing',      label: 'Posts'      },
-      { path: Path.FORUM_GROUPS_test_members,      label: 'Members'      },
-    ];
+  public navPaths: NavPath[] = [
+    { path: 'group-post-redirect-testing', label: 'Posts' },
+    { path: Path.FORUM_GROUPS_test_members, label: 'Members' },
+  ];
 
-    constructor() {
-      this.router.events.pipe(
-        filter(event => event instanceof NavigationEnd),
-        takeUntilDestroyed()
-      ).subscribe((event: NavigationEnd) => {
-        this.currentPath.set(event.urlAfterRedirects);
-      });
+  constructor() {
+    const id = this.route.snapshot.queryParamMap.get('id');
+    if(!id) return;
+
+    const dto: GetGroupReqDTO = {
+      groupId: Number(id),
+      dataRequest : {
+        includeMembers: true,
+        includePosts: true
+      }
     }
+    this.groupsService.getGroup(dto).subscribe({
+      next: (res) => {
+        this.group.set(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      takeUntilDestroyed()
+    ).subscribe((event: NavigationEnd) => {
+      this.currentPath.set(event.urlAfterRedirects);
+    });
+  }
 
-    public onNavSelectChange(event: Event) {
-      var select = event.target as HTMLSelectElement;
-      this.router.navigate([select.value]);
-    }
+  public onNavSelectChange(event: Event) {
+    var select = event.target as HTMLSelectElement;
+    this.router.navigate([select.value]);
+  }
 }
