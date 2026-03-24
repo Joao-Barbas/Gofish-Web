12160480036f99350390d06ffb953de3f2ebce06
Ricardo Reis <48648481+RicardoNogueiraReis@users.noreply.github.com>
2026-03-24 12:43:08 +0000

    Report page more in line with Figma mockup + Pin preview from Profile is done
    Pin preview design was reused from the one that appears in Profile Pin listing

--- angular/src/app/shared/components/gf-card-pin-preview/gf-card-pin-preview.component.ts
+++ angular/src/app/shared/components/gf-card-pin-preview/gf-card-pin-preview.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { Species } from '../../enums/species.enum';
+import { Bait } from '../../enums/bait.enums';
+import { AccessDifficulty } from '../../enums/access-difficulty.enums';
+import { Seabed } from '../../enums/seabed.enum';
+import { WarningKind } from '../../enums/warning-kind.enum';
+
+export type PinType = 'catch' | 'info' | 'warning';
+
 
 @Component({
   selector: 'app-gf-card-pin-preview',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './gf-card-pin-preview.component.html',
   styleUrl: './gf-card-pin-preview.component.css',
 })
 export class GfCardPinPreviewComponent {
+  @Input() pinType: PinType = 'catch';
+  @Input() isReportedPin: boolean = false;
 
+  @Input() authorProfilePhoto: string = 'assets/images/placeholder_profile_pic.png';
+  @Input() authorName: string = '';
+  @Input() timeAgo: string = '';
+  @Input() votes: number = 0;
+  @Input() comments: number = 0;
+  @Input() reportNumber?: number;
+
+  // Catch
+  @Input() species?: Species;
+  @Input() bait?: Bait;
+  @Input() hookSize?: string;
+
+  // Info
+  @Input() accessDifficulty?: AccessDifficulty;
+  @Input() seabed?: Seabed;
+
+  // Warning
+  @Input() warningKind?: WarningKind;
 }
