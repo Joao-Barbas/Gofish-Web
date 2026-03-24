e3475ad5cad9a74e011b964abdb183b10c03a19d
Gonçalo Fontoura <150022847+xFontoura@users.noreply.github.com>
2026-03-24 11:46:33 +0000

    change portuguese text in home to eng

--- angular/src/app/features/home/home.component.ts
+++ angular/src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FlatHeaderComponent } from '@gofish/features/header/flat-header/flat-header.component';
 import { FooterComponent } from '@gofish/features/footer/footer.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [ FlatHeaderComponent, FooterComponent ],
+  imports: [FlatHeaderComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
