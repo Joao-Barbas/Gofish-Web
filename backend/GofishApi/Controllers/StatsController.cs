a1376c5ead1c0588b559daa03b11b936340c181a
DanielPalmaIT <danielfilipepalma@gmail.com>
2026-03-28 10:23:44 +0000

    Created StatsDtos for retorning right now computed Statistics

---
+++ backend/GofishApi/Controllers/StatsController.cs
@@ -0,0 +1,34 @@
+﻿using GofishApi.Data;
+using GofishApi.Models;
+using GofishApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GofishApi.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class StatsController : ControllerBase 
+{
+    private readonly ILogger<StatsController> _logger;
+    private readonly AppDbContext _db;
+    private readonly UserManager<AppUser> _userManager;
+
+    public StatsController(
+    ILogger<StatsController> logger,
+    AppDbContext db,
+    UserManager<AppUser> userManager
+)
+    {
+        _logger = logger;
+        _db = db;
+        _userManager = userManager;
+    }
+
+
+
+}
+
---
+++ backend/GofishApi/Dtos/StatisticsDtos.cs
@@ -0,0 +1,25 @@
+﻿namespace GofishApi.Dtos;
+
+public record GetPinsCreatedTodayResDTO(
+    int PinsCreatedToday
+);
+
+public record GetAverageVotesPerPinResDTO(
+    double AverageVotesPerPin
+);
+
+public record GetPinsWith15PositiveVotesResDTO(
+    int PinsWith15PositiveVotes
+);
+
+public record GetReportsWaitingReviewResDTO(
+    int ReportsWaitingReview
+);
+
+public record GetAveragePublishedPinsResDTO(
+    double AveragePublishedPins
+);
+
+public record GetActiveUsersResDTO(
+    int ActiveUsers
+);
