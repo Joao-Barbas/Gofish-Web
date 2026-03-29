eee0729b623ffce90abd4697bb111b1a10bccd2e
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-03-29 20:14:37 +0100

    Update app.routes.ts to include header & footer configs
    Updated how we get data to remember last data from parent to avoid repeating header and footer data all the time
    Updated --gf-z-overlay value to the lowest of z values
    Updated all components that used old header & footer

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
+// app.component.ts
+
 import { filter, map } from 'rxjs';
 import { NgxSonnerToaster } from 'ngx-sonner';
-import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Data, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { HeaderComponent, HeaderVariant } from "@gofish/features/header/header.component";
@@ -21,9 +23,14 @@ export class AppComponent {
 
   readonly data = toSignal(this.router.events.pipe(
     filter((event) => event instanceof NavigationEnd),
-    map(() => { let r = this.route; while (r.firstChild) r = r.firstChild; return r; }),
-    map((route) => route.snapshot.data))
-  );
+    map(() => {
+      let r: ActivatedRoute = this.route;
+      let m: Data = {};
+      while (r.firstChild) r = r.firstChild;
+      for (const seg of r.pathFromRoot) m = { ...m, ...seg.snapshot.data };
+      return m;
+    })
+  ));
 
   readonly headerVariant = computed(() => (this.data()?.['header'] as HeaderVariant) ?? 'none');
   readonly footerVariant = computed(() => (this.data()?.['footer'] as FooterVariant) ?? 'none');
