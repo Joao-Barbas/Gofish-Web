8c7a45cbc4253a694f5d8d6d9ef8887d8d84c76c
Ricardo Reis <48648481+RicardoNogueiraReis@users.noreply.github.com>
2026-04-02 13:10:23 +0100

    user-help-box now hides body text div if no text is present
    user-help-box is now on statistics

--- angular/src/app/features/statistics/children/reported-pin-page/reported-pin-page.component.ts
+++ angular/src/app/features/statistics/children/reported-pin-page/reported-pin-page.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { GetPostsPostDTO } from '@gofish/shared/dtos/get-post.dto';
+import { UserHelpBoxComponent } from "@gofish/shared/components/user-help-box/user-help-box.component";
 
 @Component({
   selector: 'gf-reported-pin-page',
-  imports: [],
+  imports: [UserHelpBoxComponent],
   templateUrl: './reported-pin-page.component.html',
   styleUrl: './reported-pin-page.component.css',
 })
 export class ReportedPinPageComponent {
-  
+
+  header: string = "Review Pin Reports";
+  body: string = "To review Pin Reports, make sure to read the individual reports made by other users and see if there's a pattern of report types that you can then use to understand why the Pin is not suitable for the platform when reading its contents.\n\nMake sure to check every part of the Pin post below to see if it falls under any report type and if so, mark any report type that users have identified on the left.\n\nYou don't need to mark the same report type more than once.";
+
+  alertText: string = "You have yet to mark the Pin post with one of the report type on the left";
+
 }
