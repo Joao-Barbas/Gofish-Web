803b6885017f494a9230c1ee04d438deeffaed8d
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-04-04 20:20:24 +0100

    Intial steps to change popup namings to popover
    Created PopoverService and PopoverController
    Deprecated PopupController and PopupService
    Touched ModalController a lil
    Moved models out of .models. -> To be deleted

--- angular/src/app/shared/models/popup.model.ts
+++ angular/src/app/shared/models/popup.model.ts
@@ -1,3 +1,11 @@
+// popup.model.ts
+
+import { PopoverKey } from '@gofish/shared/core/popover-controller';
+import { PopoverController } from '@gofish/shared/core/popover-controller';
+
+/**
+ * @deprecated Use {@link PopoverKey} instead.
+ */
 export type PopupKey = string | 'example-key-popup' // Update this when creating new popup components
   | 'header-user-popup'
   | 'header-admin-popup'
@@ -6,8 +14,13 @@ export type PopupKey = string | 'example-key-popup' // Update this when creating
   | 'cluster-preview'
   | 'group-options';
 
+/**
+ * @deprecated Instatiate {@link PopoverController} instead.
+ */
 export interface SimplePopup {
   // toggle(): void;
   // open(): void;
   // close(): void;
 }
+
+// TODO: Remove this file
