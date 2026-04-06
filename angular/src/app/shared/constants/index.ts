106a0067e16b68396a77dc5948883efa9eddf78d
Gonçalo Fontoura <150022847+xFontoura@users.noreply.github.com>
2026-04-06 09:55:05 +0100

    fixing cluster bug

--- angular/src/app/shared/constants/index.ts
+++ angular/src/app/shared/constants/index.ts
@@ -111,9 +111,9 @@ export const Api = {
 } as const;
 
 export const PIN_CONFIG = [
-  { kind: PinKind.CATCH,       color: 'var(--gf-catch-pin)', iconUrl: 'assets/images/pins-icons/Kind=Catch.png',       icon: 'pin-catch',       kindString: 'Catch' },
-  { kind: PinKind.INFORMATION, color: 'var(--gf-info-pin)', iconUrl: 'assets/images/pins-icons/Kind=Information.png', icon: 'pin-Information', kindString: 'Information' },
-  { kind: PinKind.WARNING,     color: 'var(--gf-warning-pin)', iconUrl: 'assets/images/pins-icons/Kind=Warning.png',     icon: 'pin-Warning',     kindString: 'Warning'}
+  { kind: PinKind.CATCH,       color: '#4ade80', iconUrl: 'assets/images/pins-icons/Kind=Catch.png',       icon: 'pin-catch',       kindString: 'Catch' },
+  { kind: PinKind.INFORMATION, color: '#60a5fa', iconUrl: 'assets/images/pins-icons/Kind=Information.png', icon: 'pin-Information', kindString: 'Information' },
+  { kind: PinKind.WARNING,     color: '#fbbf24', iconUrl: 'assets/images/pins-icons/Kind=Warning.png',     icon: 'pin-Warning',     kindString: 'Warning'}
 ] as const;
 
 export const BodyLengthConstraints = {
