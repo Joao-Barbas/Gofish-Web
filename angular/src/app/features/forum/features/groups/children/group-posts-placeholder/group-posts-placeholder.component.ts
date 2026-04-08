e3dad3cdaa833105a883dd7716acaead41461030
Gonçalo Fontoura <150022847+xFontoura@users.noreply.github.com>
2026-04-08 23:37:03 +0100

    group infinite scroll

--- angular/src/app/features/forum/features/groups/children/group-posts-placeholder/group-posts-placeholder.component.ts
+++ angular/src/app/features/forum/features/groups/children/group-posts-placeholder/group-posts-placeholder.component.ts
@@ -17,17 +17,31 @@ import { LoadingSpinnerComponent } from "@gofish/shared/components/loading-spinn
 export class GroupPostsPlaceholderComponent {
   private readonly groupsService = inject(GroupsService);
   private readonly route = inject(ActivatedRoute);
-  protected postsData = signal<PinDto[]>([]);
+  protected postsData = signal<PinDto[] | null>(null);
   hasMoreResults = signal(true);
   private lastTimestamp: string = new Date().toISOString();
+  isLoading = signal(false);
+
+  onScroll = () => {
+    const nearBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 200;
+
+    if (nearBottom && this.hasMoreResults() && !this.isLoading()) {
+      this.loadPosts();
+    }
+  };
 
   ngOnInit() {
     this.loadPosts();
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.onScroll);
   }
 
   loadPosts() {
     const id = Number(this.route.parent?.snapshot.paramMap.get('id'));
-    if (!id) return;
+    if (!id || this.isLoading() || !this.hasMoreResults()) return;
     const dto: GetGroupPinsReqDto = {
       groupId: id,
       maxResults: 5,
@@ -35,10 +49,7 @@ export class GroupPostsPlaceholderComponent {
     }
     this.groupsService.getGroupPosts(dto).subscribe({
       next: (res) => {
-        this.postsData.update(current => {
-          this.hasMoreResults.set(res.hasMoreResults);
-          return [...current, ...res.pins];
-        });
+        this.postsData.update(current => [...(current ?? []), ...res.pins]);
 
         if (res.pins.length > 0) {
           const lastComment = res.pins[res.pins.length - 1];
@@ -54,4 +65,5 @@ export class GroupPostsPlaceholderComponent {
   showMore() {
     this.loadPosts();
   }
+
 }
