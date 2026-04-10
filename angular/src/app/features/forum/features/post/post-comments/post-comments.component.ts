9c2f457ea8efbebea306f8c7ae140f8bdc751199
Barbas <101188565+Joao-Barbas@users.noreply.github.com>
2026-04-10 20:02:18 +0100

    Access profile by click on comment author

--- angular/src/app/features/forum/features/post/post-comments/post-comments.component.ts
+++ angular/src/app/features/forum/features/post/post-comments/post-comments.component.ts
@@ -5,13 +5,14 @@ import { CommentDto } from '@gofish/shared/dtos/pin.dto';
 import { AvatarService } from '@gofish/shared/services/avatar.service';
 import { AuthService } from '@gofish/shared/services/auth.service';
 import { PinService } from '@gofish/shared/services/pin.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UserTitleComponent } from "@gofish/shared/components/user-title/user-title.component";
 import { TimeAgoPipe } from '@gofish/shared/pipes/time-ago.pipe';
+import { Path } from '@gofish/shared/constants';
 
 @Component({
   selector: 'app-post-comments',
-  imports: [TimeAgoPipe, LoadingSpinnerComponent, UserTitleComponent],
+  imports: [TimeAgoPipe, LoadingSpinnerComponent, UserTitleComponent, RouterLink],
   templateUrl: './post-comments.component.html',
   styleUrl: './post-comments.component.css',
 })
@@ -22,6 +23,7 @@ export class PostCommentsComponent {
   userName = this.authService.getUserName();
   isAdmin = this.authService.isAdmin();
   comments = input<CommentDto[]>([]);
+  Path=Path;
 
   deleteComment(commentId: number) {
     this.router.navigate(['forum', 'delete-comment', commentId]);
